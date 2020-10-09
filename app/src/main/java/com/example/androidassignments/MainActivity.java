a07233aabb9e194078dc8ea9a2d1ec7d50aa54be
SirHappi <sri13.vinukonda@gmail.com>
2020-10-09 19:19:48 -0400

    Assignment 2 part 1 done

--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.androidassignments;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(new OnClickListener() {
+//            public void onClick(View v) {
+//                ((TextView) findViewById(R.id.textView)).setText(R.string.buttonText);
+//            }
+//        });
     }
+
+
 }
